ea7497e7e3edc7320840d3d24d786c1b48a98d01
Fajrull <fajrulm05@gmail.com>
2023-12-17 15:53:27 +0700

    feat: add responsive navbar

--- src/components/Layouts/Navbar.jsx
+++ src/components/Layouts/Navbar.jsx
@@ -1,31 +1,46 @@
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const handleToggle = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <header className="navbar">
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/about">About</NavLink>
-          </li>
-          <li>
-            <NavLink to="/projects">Projects</NavLink>
-          </li>
-          <li>
-            <NavLink to="/skills">Skills</NavLink>
-          </li>
-          <li>
-            <NavLink to="/certifications">Certifications</NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact">Contact</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header className={`navbar ${isMenuOpen ? "menu-open" : ""}`}>
+        <nav>
+          <ul className={`slide ${isMenuOpen ? "open" : ""}`}>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/projects">Projects</NavLink>
+            </li>
+            <li>
+              <NavLink to="/skills">Skills</NavLink>
+            </li>
+            <li>
+              <NavLink to="/certifications">Certifications</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Contact</NavLink>
+            </li>
+          </ul>
+          <div className="menu-toggle" onClick={handleToggle}>
+            <input type="checkbox" />
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </nav>
+      </header>
+    </>
   );
 };
 
